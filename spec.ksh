2ef695684420838dba04fd732c92dac6e86b42cb
Andrew Shapton <alshapton@gmail.com>
2019-12-23 13:10:24 +0000

    test(test): jSON parsing
    initial commit to save changes for start of JSON parsing of the API response

--- spec.ksh
+++ spec.ksh
@@ -1,1 +1,1 @@
-PKG_CONFIG_PATH="/usr/local/opt/openssl/lib/pkgconfig" crystal spec -d spec/spec_helper.cr -v
+PKG_CONFIG_PATH="/usr/local/opt/openssl/lib/pkgconfig" crystal spec  -v
--- spec/SpaceCRYST-X_spec.cr
+++ spec/SpaceCRYST-X_spec.cr
@@ -1,9 +1,30 @@
 require "./spec_helper"
+require "json"
+
+struct A
+  include JSON::Serializable
+  include JSON::Serializable::Unmapped
+end
+
 
 describe SpaceCRYST::X do
   # TODO: Write tests
 
-  it "works" do
-    false.should eq(true)
+  it "Crew is fine" do
+    i = SpaceCRYST.new
+    i.crew
+    i.response.should eq("[]") 
   end
+
+  it "Capsules is fine" do
+    i = SpaceCRYST.new
+    i.capsules
+    capsules = Array(A).from_json(i.response)
+
+    capsules.each do |ky|
+      capsule=ky.json_unmapped
+      puts capsule["capsule_serial"]
+    end
+end
+
 end
